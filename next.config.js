0f40f40991792bcaefbc60bb483aa20996dd81c7
Nama Anda <andika.prastio007@gmail.com>
2026-09-25 22:53:40 +0700

    Fix build: remove db push, bypass TS

--- next.config.js
+++ next.config.js
@@ -2,14 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-  experimental: {
-    appDir: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  experimental: { appDir: true },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 module.exports = nextConfig;
